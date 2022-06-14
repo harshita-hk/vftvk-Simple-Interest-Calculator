724b174b960baa8e2e96c715134f59454e0a24d4
harshita-hk <harshitahk01@gmail.com>
2022-06-14 07:09:35 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// function compute() {
-//   var principal = document.getElementById("principal").value;
-//   if (principal == 0 || principal < 0) {
-//     alert("Enter a positive number");
-//   }
-//   var rate = document.getElementById("rate").value;
-//   var years = document.getElementById("years").value;
-
-//   var interest = (principal * rate * years) / 100;
-
-//   var year = new Date().getFullYear() + parseInt(years);
-
-//   //   var result = document.getElementById("result").value;
-//   document.getElementById("result").innerHTML =
-//     "Interest : If you deposit <mark>" +
-//     principal +
-//     "</mark>,<br/>" +
-//     "at an interest rate of <mark>" +
-//     rate +
-//     "</mark>,<br/>" +
-//     "You will receive an amount of <mark>" +
-//     interest +
-//     "</mark>,<br/> " +
-//     "in the year <mark>" +
-//     yearInTheFuture +
-//     "</mark>";
-// }
-// function updateRate() {
-//   var rateval = document.getElementById("rate").value;
-//   document.getElementById("rate_val").innerText = rateval;
-// }
 function compute() {
   //Get the values and calculate
   var principal = parseFloat(document.getElementById("principal").value);
